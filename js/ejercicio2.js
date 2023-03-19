1bc02ec8a7d694c58f89a39322a9022cd59636b5
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-03-19 13:05:45 -0300

    Se realiza el ejercicio2

---
+++ js/ejercicio2.js
@@ -0,0 +1,37 @@
+let cuenta = {
+    titular: 'Fede',
+    saldo: 0,
+    
+    ingresar:function(){
+        let saldoEntrante = +prompt('¿Cuando saldo desea ingresar?');
+        if(!isNaN(saldoEntrante)){
+            this.saldo = this.saldo + saldoEntrante;
+            alert('Saldo actualizado!');
+        }else{
+            alert('Ingresa un monto correcto! (En caso de ingresar centavos usar el punto, Ej: 140.50)');
+        }
+    },
+    extraer:function(){
+        let extraccion = +prompt('¿Cuando saldo desea extraer?');
+        if(!isNaN(extraccion)){
+            if((this.saldo - extraccion) >= 0){
+                this.saldo = this.saldo - extraccion;
+                alert('Saldo actualizado!');
+            }else{
+                alert(`El monto que se desea extraer es mayor al disponible: ${this.saldo}`);
+            }
+            
+        }else{
+            alert('Ingresa un monto correcto! (En caso de ingresar centavos usar el punto, Ej: 140.50)');
+        }
+    },
+    informe: function(){
+        alert(`Cuenta: ${this.titular}
+               Saldo actual: ${this.saldo}`);
+    }
+}
+
+cuenta.informe();
+cuenta.ingresar();
+cuenta.extraer();
+cuenta.informe();
