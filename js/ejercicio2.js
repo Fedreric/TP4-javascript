98221edf0f5505caecb4ab28a15450ecfba71859
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-03-19 13:21:59 -0300

    Se agregan botones para una mejor funcionalidad y estilos css

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,37 +1,38 @@
 let cuenta = {
     titular: 'Fede',
     saldo: 0,
-    
-    ingresar:function(){
+
+    ingresar: function () {
         let saldoEntrante = +prompt('¿Cuando saldo desea ingresar?');
-        if(!isNaN(saldoEntrante)){
+        if (!isNaN(saldoEntrante)) {
             this.saldo = this.saldo + saldoEntrante;
             alert('Saldo actualizado!');
-        }else{
+        } else {
             alert('Ingresa un monto correcto! (En caso de ingresar centavos usar el punto, Ej: 140.50)');
         }
     },
-    extraer:function(){
+    extraer: function () {
         let extraccion = +prompt('¿Cuando saldo desea extraer?');
-        if(!isNaN(extraccion)){
-            if((this.saldo - extraccion) >= 0){
+        if (!isNaN(extraccion)) {
+            if ((this.saldo - extraccion) >= 0) {
                 this.saldo = this.saldo - extraccion;
                 alert('Saldo actualizado!');
-            }else{
+            } else {
                 alert(`El monto que se desea extraer es mayor al disponible: ${this.saldo}`);
             }
-            
-        }else{
+
+        } else {
             alert('Ingresa un monto correcto! (En caso de ingresar centavos usar el punto, Ej: 140.50)');
         }
     },
-    informe: function(){
+    informe: function () {
         alert(`Cuenta: ${this.titular}
                Saldo actual: ${this.saldo}`);
     }
 }
 
-cuenta.informe();
-cuenta.ingresar();
-cuenta.extraer();
-cuenta.informe();
+document.write(`<button onclick="cuenta.ingresar()">Ingresar</button>
+                <button onclick="cuenta.extraer()">Extraer</button>
+                <button onclick="cuenta.informe()">Informe</button>`);
+
+
